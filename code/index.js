fc2785bbb3593567c0328f56340c92215093df91
nmfortn <39635355+nmfortn@users.noreply.github.com>
2018-05-26 05:23:37 +0530

    minor label update

--- code/index.js
+++ code/index.js
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
   promise.then(function(result) {
     var jsonoutput = JSON.stringify(result);
     console.log(jsonoutput); // "Stuff worked!"
-    res.status(200).send("Output from DB (output as json) ---- " + jsonoutput);
+    res.status(200).send("Query output from DB (output format == json) " + jsonoutput);
   }, function(err) {
     console.log(err); // Error: "It broke"
   });
